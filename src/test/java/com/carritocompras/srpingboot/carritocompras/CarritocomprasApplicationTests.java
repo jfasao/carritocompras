52c20b34e772c9575c076d88ac541003aeb2e85b
jfasao <jfasao@gmail.com>
2021-10-31 16:10:23 -0300

    firts commit

---
+++ src/test/java/com/carritocompras/srpingboot/carritocompras/CarritocomprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carritocompras.srpingboot.carritocompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritocomprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
